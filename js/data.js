40d0ec53af716b9fa75afe0dc6db656f9b489aaf
heyjohnis <juni0227@nate.com>
2021-01-01 10:47:30 +0900

    [TJC-32] GIT 최초 등록

---
+++ js/data.js
@@ -0,0 +1,62 @@
+export function data(){
+
+    data = {
+        mapSpot : [
+            {name: '중국교회', left: 41, top:41, since:'1919', site:'http://www.tjc.org.cn', desc: '중국교회 소개'},
+            {name: '대만교회', left: 42.8, top:46.6, since:'1919', site:'http://www.tjc.tw', desc: '대만교회 소개'},
+            {name: '한국교회', left: 43.3, top:40, since:'1950', site:'http://www.tjc.or.jp', desc: '한국교회 소개'},
+            {name: '일본교회', left: 46.5, top:40.5, since:'1943', site:'http://', desc: '일본교회 소개'},
+            {name: '홍콩교회', left: 41, top:48, since:'1919', site:'http://', desc: '홍콩교회 소개'},
+            {name: '싱가폴교회', left: 38.9, top:60, since:'1919', site:'http://www.truejesuschurch.sg', desc: '싱가폴교회 소개'},
+            {name: '말레이지아교회', left: 38.7, top:61.5, since:'1919', site:'http://', desc: '말레이지아교회 소개'},
+            {name: '사바교회', left: 42, top:58, since:'1919', site:'http://', desc: '사바교회 소개'},
+            {name: '인도네시아', left: 41, top:64.5, since:'1919', site:'http://', desc: '인도네시아교회 소개'},
+            {name: '태국', left: 38, top:52, since:'1919', site:'http://', desc: '태국교회 소개'},
+            {name: '필리핀', left: 43, top:52.2, since:'1919', site:'http://', desc: '필리핀교회 소개'},
+            {name: '미국', left: 71, top:40, since:'1919', site:'http://www.tjc.us', desc: '미국교회 소개'},
+            {name: '캐나다', left: 65, top:30, since:'1919', site:'http://tjccanada.org', desc: '캐나다교회 소개'},
+            {name: '영국', left: 11.6, top:31, since:'1919', site:'http://www.tjc.org.uk', desc: '영국교회 소개'},
+            {name: '프랑스',left: 12, top:33, since:'1919', site:'http://', desc: '프랑스교회 소개'},
+            {name: '독일', left: 14, top:31, since:'1919', site:'http://', desc: '독일교회 소개'},
+            {name: '호주', left: 50, top:80, since:'1919', site:'http://', desc: '호주교회 소개'},
+            {name: '뉴질랜드', left: 53, top:83, since:'1919', site:'http://tjc.org.nz', desc: '뉴질랜드교회 소개'},
+        
+        ],
+        
+        mapSpot2 : [
+            {name: '강남교회', left: 34, top:26, since:'1919', site:'서울특별시 강남구 일원로3길 30 (일원동)', desc: '중국교회 소개'},
+            {name: '대방교회', left: 31, top:25, since:'1919', site:'서울특별시 동작구 알마타길 29(대방동)', desc: '대만교회 소개'},
+            {name: '동부교회', left: 34, top:23, since:'1950', site:'서울특별시 동대문구 회기로 23가길 12(회기동)', desc: '한국교회 소개'},
+            {name: '서부교회', left: 31, top:23, since:'1943', site:'서울특별시 은평구 통일로50길 2-2 (녹번동)', desc: '일본교회 소개'},
+            {name: '분당교회', left: 41, top:48, since:'1919', site:'경기도 성남시 분당구 벌말로 41 (야탑동) 성원프라자 5층 503호', desc: '홍콩교회 소개'},
+            {name: '수원교회', left: 38.9, top:60, since:'1919', site:'경기도 수원시 권선구 매송고색로533번길 7(오목천동,태산아파트)상가3동 301-304호', desc: '싱가폴교회 소개'},
+            {name: '안산교회', left: 38.7, top:61.5, since:'1919', site:'경기 안산시 상록구 사동 1528-15번지, 2층', desc: '말레이지아교회 소개'},
+            {name: '안양교회', left: 42, top:58, since:'1919', site:'경기도 안양시 만안구 석천로211번길 82 (석수동) 삼우BD 3층', desc: '사바교회 소개'},
+            {name: '오산교회', left: 41, top:64.5, since:'1919', site:'경기도 오산시 은여울로7번길 13-3 (궐동)', desc: '인도네시아교회 소개'},
+            {name: '인천교회', left: 38, top:52, since:'1919', site:'인천광역시 남동구 백범로73번길 15 (만수동)', desc: '태국교회 소개'},
+            {name: '하남교회', left: 43, top:52.2, since:'1919', site:'경기도 하남시 덕산로 68 (덕풍동)(한솔빌딩6층)', desc: '필리핀교회 소개'},
+            {name: '의정교회', left: 71, top:40, since:'1919', site:'경기도 의정부시 가능로 97번길 26', desc: '미국교회 소개'},
+            {name: '대전교회', left: 65, top:30, since:'1919', site:'대전시 동구 천동 72-1 17/3', desc: '캐나다교회 소개'},
+            {name: '보령기도소', left: 11.6, top:31, since:'1919', site:'충청남도 보령시 명천중앙길 20(명천동) 정은스카이빌 103동1003호', desc: '영국교회 소개'},
+            {name: '장항서부교회', left: 11.6, top:31, since:'1919', site:'충청남도 서천군 장항읍 장항로 91번길 17', desc: '영국교회 소개'},
+            {name: '천안교회', left: 11.6, top:31, since:'1919', site:'충청남도 천안시 서북구 쌍용대로 43 (쌍용동)(유웅선 내과 4층)', desc: '영국교회 소개'},
+            {name: '청주교회', left: 11.6, top:31, since:'1919', site:'충청북도 청주시 상당구 영운천로119번길 25 (용정동', desc: '영국교회 소개'},
+            {name: '광양교회', left: 12, top:33, since:'1919', site:'전남 광양시 광양읍 덕례리 1673-2', desc: '프랑스교회 소개'},
+            {name: '광주교회', left: 14, top:31, since:'1919', site:'광주광역시 광산구 목련로394번길 10-12 (신가동)', desc: '독일교회 소개'},
+            {name: '남원교회', left: 50, top:80, since:'1919', site:'전라북도 남원시 노송로 1261 (노암동)', desc: '호주교회 소개'},
+            {name: '목포교회', left: 53, top:83, since:'1919', site:'전라남도 목포시 복산길6번길 34 (옥암동)', desc: '뉴질랜드교회 소개'},
+            {name: '삼계교회', left: 53, top:83, since:'1919', site:'전북 임실군 삼계면 삼계리 충효로 1315-3', desc: '뉴질랜드교회 소개'},
+            {name: '수문교회', left: 53, top:83, since:'1919', site:'전라남도 장흥군 안양면 수문4길 6', desc: '뉴질랜드교회 소개'},
+            {name: '전주교회', left: 53, top:83, since:'1919', site:'전라북도 전주시 완산구 백제대로 20-37(평화동1가)', desc: '뉴질랜드교회 소개'},
+            {name: '거제교회', left: 53, top:83, since:'1919', site:'경상남도 거제시 연초면 다공2길 27', desc: '뉴질랜드교회 소개'},
+            {name: '대구교회', left: 53, top:83, since:'1919', site:'대구광역시 동구 신암로14길 3-1 (신암동)', desc: '뉴질랜드교회 소개'},
+            {name: '안동교회', left: 53, top:83, since:'1919', site:'경상북도 안동시 득심골길 32-8 (상아동)', desc: '뉴질랜드교회 소개'},
+            {name: '부산교회', left: 53, top:83, since:'1919', site:'부산광역시 금정구 서금로 37-10, (서동)', desc: '뉴질랜드교회 소개'},
+            {name: '원주교회', left: 53, top:83, since:'1919', site:'강원도 원주시 행가리1길 67 (무실동) ', desc: '뉴질랜드교회 소개'},
+            {name: '제주기도소', left: 53, top:83, since:'1919', site:'제주도 제주시 ', desc: '뉴질랜드교회 소개'},
+        ]
+    }
+    retrun data;
+
+}
+
---
+++ js/script02.js
@@ -0,0 +1,5 @@
+$('.ch_history_mn_wrap li').click(function() {
+    $('.ch_history_mn_wrap li').addClass(".active_on");
+    $('.ch_history_mn_wrap li').removeClass(".active_on");
+    return false;
+});
