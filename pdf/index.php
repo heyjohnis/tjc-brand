3db8e04b32ce881d9a2dac5bd363ea3cb89a41d5
heyjohnis <juni0227@nate.com>
2021-04-03 23:47:10 +0900

    fix TJC-94 - PDF 뷰어개선

---
+++ pdf/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TJC 도서</title>
+</head>
+<body style="padding:0; margin:0;">
+    <iframe src="web/viewer.html?fileNm=<?=$_GET['fileNm']?>" frameborder="0" width="100%" height="100%" style="height:100vh;"></iframe>
+</body>
+</html>
